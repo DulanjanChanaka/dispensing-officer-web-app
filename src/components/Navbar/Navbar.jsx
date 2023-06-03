e17554d62f788a076f8179c07c61287ec7f01428
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-06-03 16:01:20 +0530

    menu close after click at responsive mobile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,41 @@
-import React, {useState} from 'react'
-import { MenuList } from './MenuList'
-import "./Navbar.css"
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { MenuList } from './MenuList';
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked, setClicked] = useState(false)
+  const menuList = MenuList.map(({ url, title }, index) => {
+    return (
+      <li key={index}>
+        <NavLink
+          exact
+          to={url}
+          activeClassName="active"
+          onClick={() => setClicked(false)} // Close the menu when a menu item is clicked
+        >
+          {title}
+        </NavLink>
+      </li>
+    );
+  });
 
-    const menuList = MenuList.map(({url, title}, index) => {
-        return (
-         <li key={index}>
-            <NavLink exact to={url} activeClassName="active">{title}</NavLink>
-         </li>
-        )
-    }
-    )
-
-    const handleClick = () => {
-        setClicked(!clicked)
-    }
-
-
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
 
   return (
     <nav>
-        <div className='logo'>
-           <a href='/'>Govt Disp</a> 
-        </div>
-        <div className='menu-icon' onClick={handleClick}>
-            <i className={ clicked ? "fas fa-times" : "fas fa-bars"}></i>
-        </div>
-        <ul className={ clicked ? 'menu-list' : "menu-list close"}>{menuList}</ul>
+      <div className="logo">
+        <a href="/">Govt Disp</a>
+      </div>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+      <ul className={clicked ? 'menu-list' : 'menu-list close'}>{menuList}</ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
